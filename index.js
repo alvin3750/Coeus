8eff05b2854ccf27f252ef87ae9e3d51e215f030
”Alvin <alvinopolot77@gmail.com>
2021-12-20 00:21:05 +0300

    project  1

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 let firstName=prompt("What is your first name?");
 let secondName=prompt("What is your second name?");
-alert(`${firstName} ${secondName} , thanks for visiting my website.`);
+alert(`${firstName} ${secondName}, thanks for visiting my website.`);
